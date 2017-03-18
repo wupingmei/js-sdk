f4fde2d928a1f2c5fd6200a3162811aabcba6697
Robin Grass <hej@carbin.se>
2017-03-18 12:20:38 +0100

    Added flow typehints to constants.

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,21 @@
+/* @flow */
+
 /**
  *	@const string API_V1
  */
-export const API_V1 = 'v1'
+export const API_V1 : string = 'v1'
 
 /**
  *	@const array API_AVAILABLE_VERSIONS
  */
-export const API_AVAILABLE_VERSIONS = [ API_V1 ]
+export const API_AVAILABLE_VERSIONS : Array<string> = [ API_V1 ]
 
 /**
  *	@const string API_DEFAULT_VERSION
  */
-export const API_DEFAULT_VERSION = API_V1
+export const API_DEFAULT_VERSION : string = API_V1
 
 /**
  *	@const string API_ENDPOINT_URL
  */
-export const API_ENDPOINT_URL = 'https://api.360player.com'
+export const API_ENDPOINT_URL : string = 'https://api.360player.com'
